107dcf71a6d8d777a73aa38deba07735d8e0b6f0
mmcdermott18 <mmcdermott18@users.noreply.github.com>
2020-12-28 17:50:26 -0500

    content updates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     // run function on initial page load
     smoothScroll();
-    clientList();
+    tmClientList();
+    blClientList();
     // run function on resize of the window
     $(window).resize(function() {
 
@@ -11,11 +12,58 @@ $(document).ready(function() {
 
     })
 });
-function clientList() {
-  $('#clients').click(function(){
-    $('#bio').toggleClass('bio-toggle');
-    $('#client-list').toggleClass('client-toggle');
-    $('.client-plus').toggleClass('plus-spin');
+function tmClientList() {
+  var tmOpen = false;
+  var tmH = $('#tm-client-list').outerHeight();
+  $('#tm-clients').click(function(){
+    if (tmOpen == false) {
+      $('#tm-client-list').removeClass('client-toggle');
+      $('#tm-client-list').addClass('client-height');
+      $('#tm-client-list').animate({
+        height: tmH,
+      }, 250, function() {
+
+      });
+      $('#tm-client-plus').addClass('plus-spin');
+      tmOpen = true;
+    } else {
+      $('#tm-client-list').animate({
+        height: 0,
+      }, 250, function() {
+
+      });
+      tmOpen = false;
+      // $('#tm-client-list').removeClass('client-height');
+      // $('#tm-client-list').addClass('client-toggle');
+      $('#tm-client-plus').removeClass('plus-spin');
+    }
+  });
+}
+function blClientList() {
+  var blOpen = false;
+  var blH = $('#bl-client-list').outerHeight();
+  $('#bl-clients').click(function(){
+    if (blOpen == false) {
+      $('#bl-client-list').removeClass('client-toggle');
+      $('#bl-client-list').addClass('client-height');
+      $('#bl-client-list').animate({
+        height: blH,
+      }, 250, function() {
+
+      });
+      $('#bl-client-plus').addClass('plus-spin');
+      blOpen = true;
+    } else {
+      $('#bl-client-list').animate({
+        height: 0,
+      }, 250, function() {
+
+      });
+      blOpen = false;
+      // $('#tm-client-list').removeClass('client-height');
+      // $('#tm-client-list').addClass('client-toggle');
+      $('#bl-client-plus').removeClass('plus-spin');
+    }
   });
 }
 function smoothScroll() {
